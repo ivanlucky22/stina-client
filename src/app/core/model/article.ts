3ba51f4ec183ec2c3fc45fb0f61cff6ecec648c3
ivanlucky22 <ivanlucky22@gmail.com>
2018-05-12 15:14:53 +0300

    added dates support

--- src/app/core/model/article.ts
+++ src/app/core/model/article.ts
@@ -6,11 +6,15 @@ export class Article {
   public title: string;
   public body: string;
   public timestamp: number;
+  public likes: number;
+  public hates: number;
+  public owner: string;
 
-  constructor(title: string, body: string) {
+  constructor(title: string, body: string, owner: string) {
     this.title = title;
     this.body = body;
-    this.timestamp = moment().unix();
+    this.owner = owner;
+    this.timestamp = moment().unix() * 1000;
   }
 
 }
